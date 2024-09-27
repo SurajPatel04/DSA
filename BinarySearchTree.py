4a6bef1c93772bed00083ef83258594874d09c6d
SurajPatel <patelsurlko20@gmail.com>
2024-09-28 02:27:04 +0530

    tree traversal

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -1,4 +1,4 @@
-    class Node:
+class Node:
     def __init__(self, value):
         self.value = value
         self.left = None
@@ -45,15 +45,84 @@ class BinarySearchTree:
             else:
                 temp = temp.right
         return False
+    
+    def BFS(self):
+        current_node = self.root
+        queue = []
+        result = []
+        queue.append(current_node)
+        
+        while len(queue) > 0:
+            current_node = queue.pop(0)
+            result.append(current_node.value)
+            if current_node.left is not None:
+                queue.append(current_node.left)
+            if current_node.right is not None:
+                queue.append(current_node.right)
+                
+        return result
+    
+    def preOrder(self):
+        result = []
+        
+        def traverse(current_node):
+            result.append(current_node.value)
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+            
+        traverse(self.root)
+        return result
+    
+    def postOrder(self):
+        result = []
+        
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+            result.append(current_node.value)
+                
+            
+        traverse(self.root)
+        return result
 
+    def inorder(self):
+        result = []
+        
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            
+            result.append(current_node.value)
+            
+            if current_node.right is not None:
+                traverse(current_node.right)
+                
+        traverse(self.root)
+        return result
 
-tree = BinarySearchTree(2)
+tree = BinarySearchTree(47)
+tree.insert(21)
+tree.insert(76)
+tree.insert(18)
+tree.insert(27)
+tree.insert(52)
+tree.insert(82)
+
+print(tree.BFS())
+
+print("PreOrder: ", tree.preOrder())
+print("PostOrder: ", tree.postOrder())
+print("Inorder: ", tree.inorder())
 # print("With no value: ", tree.root)
 # trees = BinarySearchTree(1)
 # print("with value: ", trees.root.value)
 
-tree.insert(3)
-tree.insert(1)
+# tree.insert(3)
+# tree.insert(1)
 
 # print(tree.root.value)
 # print(tree.root.right.value)
@@ -62,9 +131,9 @@ tree.insert(1)
 
 
 
-print(tree.contain(2))
-print(tree.contain(3))
-print(tree.contain(1))
-print(tree.contain(0))
+# print(tree.contain(2))
+# print(tree.contain(3))
+# print(tree.contain(1))
+# print(tree.contain(0))
 
         
--- try.py
+++ try.py
@@ -1,138 +1,35 @@
 class Node:
-    def __init__(self, value):
+    def __init__(self,value):
         self.value = value
-        self.next = None
-        self.prev = None
-        
+        self.right = None
+        self.left = None
+    
 
-class doublyLinkedlist:
-    def __init__(self, value = None):
+class BinarySearchTree:
+    def __init__(self, value= None):
         if value is None:
-            self.head = None
-            self.tail = None
-            self.length = 0
+            self.root = None
         else:
             new_node = Node(value)
-            self.head = new_node
-            self.tail = new_node
-            self.length = 1
-            
-    def printList(self):
-        if self.head is None:
-            return None
-        
-        temp = self.head
-        while temp:
-            print(temp.value)
-            temp = temp.next
-            
-    def append(self, value):
+            self.root = new_node
+    
+    def insert(self, value):
         new_node = Node(value)
-        if self.head is None:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            self.tail.next = new_node
-            new_node.prev = self.tail
-            self.tail = new_node
-        
-        self.length += 1
-        return True
-    
-    def prepend(self, value):
-        new_node = Node(value)
-        if self.head is None:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            new_node.next = self.head
-            self.head.prev = new_node
-            self.head = new_node
-        self.length += 1
-        return True
-    
-    def popfirst(self):
-        if self.head is None:
-            return None
-        
-        temp = self.head
-        if self.length == 1:
-            self.head = None
-            self.tail = None
-        else:
-            self.head = self.head.next
-            self.head.prev = None
-        
-        temp.next = None
-        self.length -= 1 
-        
-        return temp
-    
-    def pop(self):
-        if self.head is None:
-            return None
-        
-        temp = self.tail
-        self.tail = temp.prev
-        self.tail.next = None
-        temp.prev = None
-        self.length -= 1
-        
-        if self.length == 0:
-            self.head = None
-            self.tail = None
-            
-        return temp
-    
-    def get(self, index):
-        if index < 0 or index >= self.length:
-            return None
-        temp = self.head
-        if index < self.length/2:
-            for _ in range(index):
-                temp = temp.next
-        else:
-            temp = self.tail
-            for _ in range(self.length-1, index, -1):
-                temp = temp.prev
+        if self.root is None:
+            self.root = new_node
+            return True
+        temp = self.root
+        while True:
+            if temp.value == new_node.value:
+                return False
+            if new_node.value > temp.value:
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+            else:
+                if temp.left is None:
+                    tem
                 
-        return temp
+        
     
-    def insert(self, index, value):
-        if index < 0 or index > self.length:
-            return None
-        
-        if index == 0:
-            return self.prepend(value)
-        
-        if self.length - 1 == index:
-            return self.append(value)
-        
-        new_node = Node(value)
-        temp = self.get(index-1)
-        after = temp.next
-        temp.next = new_node
-        new_node.prev = temp
-        new_node.next = after
-        after.prev = new_node
-        
-        self.length += 1
-        return True
-    
-    
-doubly = doublyLinkedlist(5)
-doubly.append(9)
-doubly.append(10)
-doubly.append(11)
-doubly.append(12)
-doubly.printList()
-
-# print(doubly.get(1))
-# print(doubly.get(4))
-
-print("Change")
-# doubly.popfirst()
-# doubly.pop()
-
-doubly.insert(2,100)
-doubly.printList()
